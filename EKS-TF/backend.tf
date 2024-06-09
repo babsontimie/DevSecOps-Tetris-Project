fd9365969a126eef1d940279d8130d86188c3e99
babsontimie <139099122+babsontimie@users.noreply.github.com>
2024-06-09 23:49:50 +0100

    Add files via upload

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "my-ews-baket1"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    key            = "devsecops-eks-bucket/EKS-TF/terraform.tfstate"
+ #   dynamodb_table = "Lock-Files"
     encrypt        = true
   }
   required_version = ">=0.13.0"
